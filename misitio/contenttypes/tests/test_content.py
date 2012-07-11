64efee1e499e5b4b017f5403c3b276363c1a258c
Luis Florez <lefway@gmail.com>
2012-07-11 19:14:16 -0430

    modificacion

--- misitio/contenttypes/tests/test_content.py
+++ misitio/contenttypes/tests/test_content.py
@@ -1,9 +1,16 @@
 import unittest2 as unittest
 
 from Products.CMFCore.utils import getToolByName
+from misitio.utilities.utility import createConcejoComunal
 
 from misitio.contenttypes.testing import\
     MISITIO_CONTENTTYPES_INTEGRATION_TESTING
+
+from plone.app.testing import TEST_USER_ID, TEST_USER_NAME
+from plone.app.testing import (
+   login, setRoles,
+   )
+#from plone.dexterity.utils import createContent
 
 
 class TestContent(unittest.TestCase):
@@ -32,9 +39,14 @@ class TestContent(unittest.TestCase):
         self.assertTrue('concejo_comunal' in existing)
         self.assertTrue('miembro' in existing)
 
-#    def test_consejo_comunal_allowed_content_types(self):
-#        folder = self.types.objectIds()
-# 
-#        allowed_types = ('miembro', ) 
-#        elementos = folder[concejo_comunal].getImmediatelyAddableTypes(allowed_types)
-#        self.assertTrue('miembro' in elementos)
+
+    def test_consejo_comunal_allowed_content_types(self):
+
+       setRoles(self.portal, TEST_USER_ID, ['Manager'])
+       login(self.portal, TEST_USER_NAME)
+       createConcejoComunal(self.portal,'tipitiripe')
+       self.assertTrue('tipitiripe' in self.portal, 'tipitiripe ConcejoComunal not created.')
+       setRoles(self.portal, TEST_USER_ID, ['Member'])
+       cc = self.portal['tipitiripe']
+       self.assertEqual(cc.getImmediatelyAddableTypes(), ['File', 'Image', 'Link', 'miembro'])
+
